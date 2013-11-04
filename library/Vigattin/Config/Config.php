75fcb380317b4fe6e63bb2f99eef51853669be52
Resty Rizal <resty_rizal@live.com>
2013-11-04 22:36:21 +0800

    fix config

--- library/Vigattin/Config/Config.php
+++ library/Vigattin/Config/Config.php
@@ -7,19 +7,19 @@ namespace Vigattin\Config;
 
 class Config {
     
-    const DEFAULT_CONFIG_FILE = 'config/config.php';
+    const DEFAULT_CONFIG_FILE = '../../../config/config.php';
 
     public $config;
     
     public function __construct($config = '') {
-        //$this->config = array();
-        //if($config === '') $config = include __DIR__.'/../'.self::DEFAULT_CONFIG_FILE;
-        //if(is_array($config)) $this->mergeConfigArray($config);
-        //if(is_file($config)) $this->mergeConfigFile ($config);
+        $this->config = array();
+        if($config === '') $config = include __DIR__.'/'.self::DEFAULT_CONFIG_FILE;
+        if(is_array($config)) $this->mergeConfigArray($config);
+        elseif(is_file($config)) $this->mergeConfigFile ($config);
     }
     
     public function mergeConfigArray($configArray) {
-        if(!is_array($configArray)) array_merge ($this->config, $configArray);
+        if(is_array($configArray)) $this->config = array_merge($this->config, $configArray);
         return $this->config;
     }
     
