81ab008623bc96252ea8ac5719aab53887100361
Resty Rizal <resty_rizal@live.com>
2013-11-04 23:53:10 +0800

    add sample config

---
+++ config/sample.config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'facebook' => array(
+        'appId' => '',
+        'appSecret' => '',
+    ),
+    'connect' => array(
+        'apiUrl' => '',
+        'sslCheckCertificate' => TRUE,
+        'authDomainLoginUrl' => '',
+        'authDomainSecureLoginUrl' => '',
+        'authDomainLogoutUrl' => '',
+        'authDomainSecureLogoutUrl' => '',
+        'requestExpire' => 180,
+        'apiKey' => '',
+    ),
+);
