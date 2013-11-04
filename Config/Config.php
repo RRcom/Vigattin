ecc08157a1813e8b02d72904b004b025fbd1be7f
Resty <resty_rizal@live.com>
2013-11-04 14:39:45 +0800

    revising directory

--- Config/Config.php
+++ Config/Config.php
@@ -1,31 +1,4 @@
 <?php
-namespace Vigattin\Config;
-
-class Config {
+return array(
     
-    public $config;
-    
-    public function __construct($config = '') {
-        $this->config = array();
-        if(!is_array($config)) $this->mergeConfigArray($config);
-    }
-    
-    public function mergeConfigArray($configArray) {
-        if(!is_array($configArray)) array_merge ($this->config, $configArray);
-        return $this->config;
-    }
-    
-    public function mergeConfigFile($configFile) {
-        if(is_file($configFile)) {
-            $config = include $configFile;
-            $this->mergeConfigArray($config);
-        }
-        return $config;
-    }
-    
-    public function getConfig() {
-        return $this->config;
-    }
-    
-}
-
+);
--- Connect/Connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Vigattin\Connect;
-
-class Connect {
-    
-}
