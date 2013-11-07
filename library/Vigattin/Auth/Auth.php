4760db49e0f5415f134c4833c86e7adffcebe656
Resty <resty_rizal@live.com>
2013-11-07 12:02:49 +0800

    fix URLtools in Auth

--- library/Vigattin/Auth/Auth.php
+++ library/Vigattin/Auth/Auth.php
@@ -27,7 +27,7 @@ class Auth {
      */
     
     public function getLoginUrl($redirect = '', $secure = FALSE) {
-        if($redirect === '') $redirect = URLtools::getCurrentUrl();
+        if($redirect === '') $redirect = URLTools::getCurrentUrl();
         if($secure) return $this->config->config['connect']['authDomainSecureLoginUrl'].'?redirect='.urlencode(urldecode($redirect));
         else return $this->config->config['connect']['authDomainLoginUrl'].'?redirect='.urlencode(urldecode($redirect));
     }
